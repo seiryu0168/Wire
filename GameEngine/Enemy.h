ca1ea403136393cf0f3146b24c68542c616b869d
成龍 長尾 <210215@jc-21.jp>
2022-12-09 01:13:40 +0900

    コメント

--- GameEngine/Enemy.h
+++ GameEngine/Enemy.h
@@ -36,31 +36,32 @@ public:
     virtual void Attack()=0;
     
     bool IsVisible(float angle,float range);//指定した視野内にプレイヤーがいるかどうか : 視角、見える距離
-    void ChangeState(EnemyState* state);
-    void SetviewRange(float range) { enemyParameter_.viewRange = range; }
-    void SetviewAngle(float angle) { enemyParameter_.viewAngle = angle; }
-    void SetToPlayerVec(XMVECTOR vec) { enemyParameter_.toPlayerVec = vec; }
-    void SetPositionVec(XMVECTOR vec) { enemyParameter_.vPosition = vec; }
-    void SetFrontVec(XMVECTOR vec) { enemyParameter_.frontVec = vec; }
-    void SetMatrixX(XMMATRIX mat) { enemyParameter_.matX = mat; }
-    void SetMatrixY(XMMATRIX mat) { enemyParameter_.matY = mat; }
-    void SetPlayerPointer(Player* pointer) { enemyParameter_.pPlayer = pointer; }
-    void SetLife(int life) { enemyParameter_.life = life; }
-    void DecreaseLife(int decRate) { enemyParameter_.life -= decRate; }
-    void IncreaseLife(int incRate) { enemyParameter_.life += incRate; }
+    
+    void ChangeState(EnemyState* state);                                         //エネミーの状態変更
+    void SetviewRange(float range) { enemyParameter_.viewRange = range; }        //見える距離設定
+    void SetviewAngle(float angle) { enemyParameter_.viewAngle = angle; }        //視角設定
+    void SetToPlayerVec(XMVECTOR vec) { enemyParameter_.toPlayerVec = vec; }     //プレイヤーに向かうベクトルの設定
+    void SetPositionVec(XMVECTOR vec) { enemyParameter_.vPosition = vec; }       //ポジションベクトルの設定
+    void SetFrontVec(XMVECTOR vec) { enemyParameter_.frontVec = vec; }           //前方向ベクトルの設定
+    void SetMatrixX(XMMATRIX mat) { enemyParameter_.matX = mat; }                //X軸の回転行列
+    void SetMatrixY(XMMATRIX mat) { enemyParameter_.matY = mat; }                //Y軸の回転行列
+    void SetPlayerPointer(Player* pointer) { enemyParameter_.pPlayer = pointer; }//プレイヤーのポインター
+    void SetLife(int life) { enemyParameter_.life = life; }                      //エネミーのライフ設定
+    void DecreaseLife(int decRate) { enemyParameter_.life -= decRate; }          //ライフを減らす
+    void IncreaseLife(int incRate) { enemyParameter_.life += incRate; }          //ライフを増やす
 
-    float GetViewAngle() { return enemyParameter_.viewAngle; }
-    float GetViewRange() { return enemyParameter_.viewRange; }
-    XMMATRIX GetMatrixX() { return enemyParameter_.matX; }
-    XMMATRIX GetMatrixY() { return enemyParameter_.matY; }
-    XMVECTOR GetToPlayerVec() { return enemyParameter_.toPlayerVec; }
-    XMVECTOR GetPositionVec() { return enemyParameter_.vPosition; }
-    XMVECTOR GetUpVec() { return enemyParameter_.upVec; }
-    XMVECTOR GetFrontVec() { return enemyParameter_.frontVec; }
-    Player* GetPlayerPointer() { return enemyParameter_.pPlayer; }
-    bool GetIsList() { return enemyParameter_.isTargetList; }
-    int GetLife() { return enemyParameter_.life; }
-    EnemyState* GetEnemyState() { return pEnemyState_; }
+    float    GetViewAngle() { return enemyParameter_.viewAngle; }       //見える距離取得
+    float    GetViewRange() { return enemyParameter_.viewRange; }       //視角取得
+    XMMATRIX GetMatrixX() { return enemyParameter_.matX; }              //X軸回転行列取得
+    XMMATRIX GetMatrixY() { return enemyParameter_.matY; }              //Y軸回転行列取得
+    XMVECTOR GetToPlayerVec() { return enemyParameter_.toPlayerVec; }   //プレイヤーに向かうベクトル取得
+    XMVECTOR GetPositionVec() { return enemyParameter_.vPosition; }     //ポジションベクトル取得
+    XMVECTOR GetUpVec() { return enemyParameter_.upVec; }               //上方向ベクトル取得
+    XMVECTOR GetFrontVec() { return enemyParameter_.frontVec; }         //前方向ベクトル取得
+    Player*  GetPlayerPointer() { return enemyParameter_.pPlayer; }     //プレイヤーのポインター取得
+    bool     GetIsList() { return enemyParameter_.isTargetList; }       //エネミーがプレイヤーの認識リストに入ってるかどうか
+    int      GetLife() { return enemyParameter_.life; }                 //エネミーのライフ取得
+    EnemyState* GetEnemyState() { return pEnemyState_; }                //エネミーの状態取得
 
 };
 
