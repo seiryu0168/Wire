4f16f70315f9d3c5983abbc8bb1a99be5af7da29
成龍 長尾 <210215@jc-21.jp>
2023-04-15 22:55:21 +0900

    EnemyBallモデルのUV値を入れるとm128_i8に妙なデータが入って頂点情報がちゃんと入らない

--- GameEngine/Engine/DirectX_11/FbxParts.cpp
+++ GameEngine/Engine/DirectX_11/FbxParts.cpp
@@ -271,30 +271,32 @@ HRESULT FbxParts::InitVertex(fbxsdk::FbxMesh* mesh)
 		}
 #endif
 	}
-	int UVCount = mesh->GetTextureUVCount();
 	FbxLayerElementUV* pUV = mesh->GetLayer(0)->GetUVs();
-	if (UVCount > 0 && pUV->GetMappingMode() == FbxLayerElement::eByControlPoint)
+	int UVNum = pUV->GetDirectArray().GetCount();
+	int UVCount = mesh->GetTextureUVCount();
+	int indexNum = pUV->GetIndexArray().GetCount();
+
+	if (pUV->GetMappingMode() == FbxLayerElement::eByControlPoint)
 	{
 		for (int i = 0; i < UVCount; i++)
 		{
 			FbxVector2  uv = pUV->GetDirectArray().GetAt(i);
-			pVertices_[i].uv = XMVectorSet((float)uv.mData[0], (float)(1.0f - uv.mData[1]), 0.0f, 0.0f);
+			pVertices_[i].uv = XMVectorSet((float)uv.mData[0], (float)(uv.mData[1]), 0.0f, 0.0f);
 		}
 	}
-	if (UVCount > 0 && pUV->GetMappingMode() == FbxLayerElement::eByPolygonVertex)
-	{
-			int indexCount = pUV->GetIndexArray().GetCount();
-		if (((FbxLayerElement*)pUV)->GetReferenceMode() == FbxLayerElement::eIndexToDirect)
-		{
-			//FbxLayerElement indeces = 
-			for (int i = 0; i < UVCount; i++)
-			{
-				int ind= pUV->GetIndexArray().GetAt(i);
-				FbxVector2  uv = pUV->GetDirectArray().GetAt(i);
-				pVertices_[i].uv = XMVectorSet((float)uv.mData[0], (float)(1.0f - uv.mData[1]), 0.0f, 0.0f);
-			}
-		}
-	}
+	//if (pUV->GetMappingMode() == FbxLayerElement::eByPolygonVertex)
+	//{
+	//	if (((FbxLayerElement*)pUV)->GetReferenceMode() == FbxLayerElement::eIndexToDirect)
+	//	{
+	//		//FbxLayerElement indeces = 
+	//		for (int i = 0; i < UVCount; i++)
+	//		{
+	//			int ind= pUV->GetIndexArray().GetAt(i);
+	//			FbxVector2  uv = pUV->GetDirectArray().GetAt(ind);
+	//			pVertices_[i].uv = XMVectorSet((float)uv.mData[0], (float)(uv.mData[1]), 0.0f, 0.0f);
+	//		}
+	//	}
+	//}
 
 	D3D11_BUFFER_DESC bd_vertex;
 	bd_vertex.ByteWidth = sizeof(VERTEX) * vertexCount_;
