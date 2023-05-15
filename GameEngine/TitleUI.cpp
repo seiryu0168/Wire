7f8ba052373779b18d2e3d2db58d95ef0a500d60
成龍 長尾 <210215@jc-21.jp>
2023-05-15 13:29:05 +0900

    UIのデータ読み込み中

--- GameEngine/TitleUI.cpp
+++ GameEngine/TitleUI.cpp
@@ -3,17 +3,23 @@
 #include"InterSceneData.h"
 #include"Engine/DirectX_11/Input.h"
 #include"Engine/SceneManager.h"
+#include<fstream>
+#include<sstream>
+#include"Engine/ResourceManager/json.hpp"
 #include"Easing.h"
 
+using json = nlohmann::json;
 
 namespace
 {
+	static const std::string UI_IMAGE_FILE = "StageSelectUIImage.json";
+	static const std::string BUTTON_LIST_NAME = "StageSelectUIImageList";
 	static const int INTERVAL = 1;
 	static const int MAX_BUTTON = 5;
 	static const int DELAY = 90;
 	static const int MOVE = 500;
 	static const int MAX_MOVE_TIME = 11;
-	static const XMFLOAT2 BUTTON_FRAME_POS = { 800.0f,-500.0f };
+	static const XMFLOAT3 BUTTON_FRAME_POS = { -1400.0f,0.0f,0.0f };
 }
 
 
@@ -28,7 +34,7 @@ TitleUI::TitleUI(GameObject* parent)
 
 {
 
-	//ReadFile()
+	ReadFile(UI_IMAGE_FILE);
 }
 
 TitleUI::~TitleUI()
@@ -37,30 +43,11 @@ TitleUI::~TitleUI()
 
 void TitleUI::Initialize()
 {
-	//タイトル画像読み込み
-	hPictTitle_ = ImageManager::Load("Assets\\TitleImage2.png");
-	assert(hPictTitle_ >= 0);
-
-	//ボタン画像読み込み
-	for (int i = 0; i < MAX_BUTTON; i++)
-	{
-		button_ btn;
-		btn.hPict_ = ImageManager::Load("Assets\\PlayImage.png");
-		assert(btn.hPict_ >= 0);
-		btn.position_ = { -800.0f,i * (float)(-MOVE),0.0f};
-		ImageManager::SetImagePos(btn.hPict_,btn.position_);
-		buttonList_.push_back(btn);
-	}
-	hPictButtonFrame_ = ImageManager::Load("Assets\\ButtonFrame.png");
-	assert(hPictButtonFrame_ >= 0);
 	
-	ImageManager::SetImagePos(hPictButtonFrame_, XMFLOAT3(-800, 0, 0));	
 }
 
 void TitleUI::Update()
 {
-	
-	
 	switch (uiMode_)
 	{
 	case UI_MODE::MODE_INPUT:
@@ -124,16 +111,12 @@ void TitleUI::Update()
 
 void TitleUI::MoveButton(int num)
 {
-	int deltaPos = 0;
-	if (num==-1||num==MAX_BUTTON)
-	{
-		return;
-	}
+	int deltaPos = (MOVE / (MAX_MOVE_TIME-1)) * buttonMove_;
+
 	for (int i = 0; i < MAX_BUTTON; i++)
 	{
-		deltaPos = (MOVE / (MAX_MOVE_TIME-1)) * buttonMove_;
 		buttonList_[i].position_.y += deltaPos;
-		ImageManager::SetImagePos(buttonList_[i].hPict_,
+		ImageManager::SetImagePos(buttonList_[i].hButtonPict_,
 			XMFLOAT3(buttonList_[i].position_.x,
 				buttonList_[i].position_.y,
 				0));
@@ -147,9 +130,16 @@ void TitleUI::MoveButton(int num)
 
 }
 
+bool TitleUI::IsLimit(int buttonNum)
+{
+	if(buttonNum > 0 || buttonNum < (MAX_BUTTON - 1))
+	return true;
+	else
+		return false;
+}
+
 void TitleUI::Input()
 {
-
 		buttonMove_ = 0;
 		if (Input::GetLStick_Y() >= 0.7f)
 		{
@@ -183,49 +173,53 @@ void TitleUI::Move()
 
 		MoveButton(buttonNum_);
 }
-void TitleUI::MoveToEnd(int num)
-{
-	for (int i = 0; i < MAX_BUTTON; i++)
-	{
-		buttonList_[i].position_.y = (num+1)*MOVE*i;
-		ImageManager::SetImagePos(buttonList_[i].hPict_,
-			XMFLOAT3(buttonList_[i].position_.x,
-				buttonList_[i].position_.y,
-				0));
-	}
-}
-
-int TitleUI::IsEndorStart(int buttonNum)
-{
-	if (buttonNum == 0 || buttonNum == (MAX_BUTTON - 1))
-		return 1;
-	else
-		return false;
-}
 
 
 void TitleUI::ReadFile(std::string fileName)
 {
-	////カレントディレクトリ取得
-	//WCHAR currentDir[MAX_PATH];
-	//GetCurrentDirectory(MAX_PATH, currentDir);
-	//
-	//WCHAR path[FILENAME_MAX];
-	//
-	//if (SetCurrentDirectory(L"Assets") == ERROR_FILE_NOT_FOUND)
-	//{
-	//	return;
-	//}
-	//std::ifstream fileReader;
-	//fileReader.open(fileName.c_str());
-	//if (fileReader.good())
-	//{
-	//	enemyData_ = new json;
-	//	*enemyData_ = json::parse(fileReader);
-	//	//fileReader >> (*enemyData_);
-	//}
-	//fileReader.close();
-	//SetCurrentDirectory(currentDir);
+	//カレントディレクトリ取得
+	WCHAR currentDir[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, currentDir);
+	
+	WCHAR path[FILENAME_MAX];
+	
+	if (SetCurrentDirectory(L"Assets") == ERROR_FILE_NOT_FOUND)
+	{
+		return;
+	}
+	std::ifstream fRead;
+	fRead.open(fileName.c_str());
+	if (fRead.good())
+	{
+		fileReader_ = new json;
+		*fileReader_ = json::parse(fRead);
+	}
+	fRead.close();
+	SetCurrentDirectory(currentDir);
+}
+
+void TitleUI::LoadImageFile()
+{
+
+
+	//タイトル画像読み込み
+	hPictTitle_ = ImageManager::Load("Assets\\TitleImage2.png");
+	assert(hPictTitle_ >= 0);
+
+	//ボタン画像読み込み
+	for (auto elem :fileReader_[0][BUTTON_LIST_NAME].items())
+	{
+		button_ btn;
+		btn.hButtonPict_ = ImageManager::Load("Assets\\PlayImage.png");
+		assert(btn.hButtonPict_ >= 0);
+		btn.position_ = { -1400.0f,i * (float)(-MOVE),0.0f };
+		ImageManager::SetImagePos(btn.hButtonPict_, btn.position_);
+		buttonList_.push_back(btn);
+	}
+	hPictButtonFrame_ = ImageManager::Load("Assets\\ButtonFrame.png");
+	assert(hPictButtonFrame_ >= 0);
+
+	ImageManager::SetImagePos(hPictButtonFrame_, BUTTON_FRAME_POS);
 }
 
 void TitleUI::Release()
--- GameEngine/TitleUI.h
+++ GameEngine/TitleUI.h
@@ -1,7 +1,6 @@
 #pragma once
 #include"Engine/GameObject/GameObject.h"
-#include"Engine/ResourceManager/json.hpp"
-using json = nlohmann::json;
+
 class TitleUI : public GameObject
 {
 private:
@@ -12,11 +11,12 @@ private:
 	};
 	struct button_
 	{
-		int hPict_;
+		int hButtonPict_;
+		int hMissionPict_;
 		XMFLOAT3 position_;
 	};
 	short buttonMove_;
-	json fileReader_;
+	json* fileReader_;
 	bool isMoveEnd_;
 	bool canPushButton_;
 	UI_MODE uiMode_;
@@ -35,8 +35,8 @@ public:
 	void Initialize() override;
 	void Update() override;
 	void MoveButton(int num);
-	void MoveToEnd(int buttonNum);
-	int IsEndorStart(int buttonNum);
+	bool IsLimit(int buttonNum);
 	void ReadFile(std::string fileName);
+	void LoadImageFile();
 	void Release() override;
 };
